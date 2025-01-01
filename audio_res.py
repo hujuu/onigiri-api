9671c3836bc80b2c3943ae5eb35ca51e73f47f7b
hujuu <1961946+hujuu@users.noreply.github.com>
2025-01-01 17:35:00 +0900

    Create audio_res.py

---
+++ audio_res.py
@@ -0,0 +1,22 @@
+import base64
+from openai import OpenAI
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-audio-preview",
+    modalities=["text", "audio"],
+    audio={"voice": "alloy", "format": "wav"},
+    messages=[
+        {
+            "role": "user",
+            "content": "こんにちは！犬と猫の見分け方を教えてください"
+        }
+    ]
+)
+
+print(completion.choices[0])
+
+wav_bytes = base64.b64decode(completion.choices[0].message.audio.data)
+with open("dog.wav", "wb") as f:
+    f.write(wav_bytes)
